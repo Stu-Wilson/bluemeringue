f2953e64acd5d903d22aa30a861177067e80d708
Stu <stuartw87@hotmail.com>
2023-08-14 19:57:10 +0100

    changes to contact page

--- .next/static/chunks/237.2edcc235580705a1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[237],{9237:function(_){_.exports={style:{fontFamily:"'__Inter_9d0077', '__Inter_Fallback_9d0077'",fontStyle:"normal"},className:"__className_9d0077"}}}]);
--- .next/static/chunks/app/error/page-23424fa6c968f3b6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[933,301,327,281,966],{9944:function(n,e,u){Promise.resolve().then(u.t.bind(u,4090,23))}},function(n){n.O(0,[90,17,744],function(){return n(n.s=9944)}),_N_E=n.O()}]);
